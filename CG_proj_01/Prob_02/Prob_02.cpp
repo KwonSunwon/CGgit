7ef1503d0164ab6c237c3518c8f8d5f5b660402f
KwonSunwon <kso00011147@gmail.com>
2022-09-22 00:09:36 +0900

    Add prob_02

---
+++ CG_proj_01/Prob_02/Prob_02.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+
+GLclampf g_color[4] = {0.0f, 0.0f, 0.0f, 1.0f};
+
+void main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(250, 250);
+    glutCreateWindow("Example1");
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Unable to initialize GLEW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+        std::cout << "GLEW Initialized\n";
+
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(Reshape);
+    glutMainLoop();
+}
+
+GLvoid drawScene()
+{
+    glClearColor(g_color[0], g_color[1], g_color[2], g_color[3]);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+}
